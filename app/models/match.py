606384c5a0c0b89fa63fa777a8e2fd1fb3f1a5f2
FilipeLacerda738 <filipelacerda122@gmail.com>
2026-05-30 10:13:42 -0300

    alteracoes gerais

--- app/models/match.py
+++ app/models/match.py
@@ -21,9 +21,8 @@ class Match(Base):
 
   begin_at = Column(DateTime(timezone=True), nullable=True)
   
-  stream_url = Column(String(255), nullable=True)
+  stream_url = Column(String, nullable=True)
   number_of_games = Column(Integer, server_default="3", nullable=True)
-  created_at = Column(DateTime(timezone=True), server_default=func.now())
 
   created_at = Column(DateTime(timezone=True), server_default=func.now())
   updated_at = Column(DateTime(timezone=True), onupdate=func.now())
